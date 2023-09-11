dcc09253c6dc20ea063061cdae3c4154938b1065
Alex Taveras-Crespo <alexmanuelt@hotmail.com>
2023-09-11 16:20:41 -0400

    Mobile styling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,13 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		screens: {
+			'sm': '640px',
+			'md': '768px',
+			'lg': '1024px',
+			'xl': '1280px',
+			'2xl': '1536px',
+		},
 		extend: {
 			animation: {
 				text: 'text 15s ease infinite',

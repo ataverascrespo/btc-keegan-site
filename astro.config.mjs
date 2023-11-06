0dd3b72d53d7a0b841d5245308a012f06519b412
Alex Taveras-Crespo <alexmanuelt@hotmail.com>
2023-11-06 11:47:57 -0500

    Config updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify/functions";
-
 import alpinejs from "@astrojs/alpinejs";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()],
+  integrations: [tailwind(), alpinejs(), react()],
   output: 'server',
   adapter: netlify()
 });

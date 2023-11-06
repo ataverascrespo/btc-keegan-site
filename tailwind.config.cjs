5d1fc9e6961a55dec00b3387f591441c9ce7e00b
Alex Taveras-Crespo <alexmanuelt@hotmail.com>
2023-11-06 09:59:27 -0500

    Add xxs breakpoint for more precise mobile styling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,7 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		screens: {
+			xxs: '390px',
 			xs: '475px',
 			sm: '640px',
 			md: '768px',
